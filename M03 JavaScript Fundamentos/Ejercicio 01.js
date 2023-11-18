edd26752300336e138d32e05d02005db3d62bb50
Roibiz <pachecoroibi@gmail.com>
2023-11-18 11:14:30 -0300

    ejercicios modulo 3 prepcourse

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,23 +5,17 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
-
+const nuevoString ='hola';
 // Crea una variable de tipo number.
-const nuevoNumero = null;
-
+const nuevoNumero = 42;
 // Crea una variable de tipo boolean.
-const nuevoBoolean = null;
-
+const nuevoBoolean = 3 == '3';
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - null === 3;
-
+const nuevaResta = 10 - 7 === 3;
 // Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * null === 40;
-
+const nuevaMultiplicacion = 10 * 4 === 40;
 // Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5 === null;
-
+const nuevoModulo = 21 % 5 === 1;
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    nuevoString,
@@ -31,3 +25,4 @@ module.exports = {
    nuevaMultiplicacion,
    nuevoModulo,
 };
+
--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,43 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
-
+devolverString("hola")
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x + y;
 }
-
+suma(2, 8);
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y;
 }
-
+resta(5, 4);
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y;
 }
-
+divide(12, 2)
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return  x * y;
 }
+multiplica(23, 4)
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x % y;
 }
+obtenerResto(78, 4)
+// resultado 2
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
